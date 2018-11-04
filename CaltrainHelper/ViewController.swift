497167aafbe53e23540f3d8fd170fe52db027bd2
Jiaqi Chen <jackychen901010@gmail.com>
2018-11-04 07:59:04 -0800

    Initial Commit

---
+++ CaltrainHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CaltrainHelper
+//
+//  Created by Jiaqi Chen on 11/4/18.
+//  Copyright © 2018 Jiaqi Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
